c5ae408b4e02e59b9cc3d51d3b0e06a9230f72fd
Yooaknow <dbdkfma1228@naver.com>
2025-10-27 02:05:21 +0900

    feat:자동차 이름과  시도 횟수 입력 기능 추가
    자동차 이름과 시도 횟수를 입력받는 기능을 구현했습니다

---
+++ src/input.js
@@ -0,0 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
+
+
+export const getCarNames = async () => {
+    const input = await Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분\n"
+    );
+
+    return input.split(",").map((name)=>name.trim());
+};
+
+    export const getTryCount = async () => {
+  const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+  return Number(input);
+};
+
