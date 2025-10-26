40c1572e38ce852040cf7495c897a08b54f8bca7
Yooaknow <dbdkfma1228@naver.com>
2025-10-27 02:08:57 +0900

    docs: 입력 처리 단계 구현 항목 체크 완료
    구현 체크리스트의 자동차 이름과 시도 횟수 입력 관련 기능을 체크 상태로 변경했습니다.

--- src/input.js
+++ src/input.js
@@ -3,7 +3,7 @@ import { Console } from "@woowacourse/mission-utils";
 
 export const getCarNames = async () => {
     const input = await Console.readLineAsync(
-        "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분\n"
+        "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n"
     );
 
     return input.split(",").map((name)=>name.trim());
